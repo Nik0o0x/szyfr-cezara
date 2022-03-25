ef00b855562e55ad306e3bf8afd6d4786462c25d
3108917 <3108917@TE-GDA-D-D12C01>
2022-03-25 13:00:56 +0100

    Dodaj pliki projektów.

---
+++ szyfr cezara/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Szyfr_Cezara
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj wartość przesunięcia:");
+            int szyfr;
+            szyfr = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Podaj teskt do zmienienia:");
+            foreach (var c in Console.ReadLine())
+            {
+                Console.Write(Convert.ToChar((c - 'a' + szyfr) % ('z' - 'a' + szyfr) + 'a'));
+            }
+            Console.WriteLine();
+            Console.ReadKey(true);
+        }
+    }
+}
